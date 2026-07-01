678186dcfdf63374bece9fa7cb7339949688578c
Soumya Siricilla <soumyasircillaa@gmail.com>
2026-07-02 00:53:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Portfolio Loaded");
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+
+    navbar.classList.toggle("active");
+
+};
